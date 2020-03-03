dd751adf9bc908296927acbd90f68d93162353da
Ananta Yudica <nta040689@gmail.com>
2020-03-03 11:02:33 +0700

    Added function w and v Added var x_est and p_est Updated var from x, u, C, and CT to x0, u0, H, and HT Updated value Q with 0.1 Fixed flow loop per data

--- main.R
+++ main.R
@@ -11,35 +11,48 @@ fu <- function(u)
   return (0)
 }
 
-n <- 10
-x <- 0
-u <- 0
+fw <- function(w)
+{
+  return (0)
+}
 
-p <- 1
+fv <- function(v)
+{
+  return (0)
+}
+
+n <- 10
+x0 <- 1
+u0 <- 0
+
+x_est <- 0
+p_est <- 1
 
 A <- matrix(1, 1, 1)
 AT <- t(A)
 
 B <- matrix(1, 1, 1)
 
-C <- matrix(1, 1, 1)
-CT <- t(C)
+H <- matrix(1, 1, 1)
+HT <- t(H)
 
 I <- matrix(0, 1, 1)
 diag(I) <- 1
 
-Q <- 0
+Q <- 0.1
+
+#initial data
 
 #create vector x with fx
-vec_x <- c(x)
+vec_x <- c(x0)
 lapply(2:n, function(i){vec_x[i] <<- fx(vec_x[i-1])})
 
-#create vector xn_ with fx
-vec_xn_ <- c(fx(x))
-lapply(2:n, function(i){vec_xn_[i] <<- fx(vec_xn_[i-1])})
+#create vector xn real with fx
+vec_xn_real <- c(fx(x0))
+lapply(2:n, function(i){vec_xn_real[i] <<- fx(vec_xn_real[i-1])})
 
 #create vector u with fu
-vec_u <- c(u)
+vec_u <- c(u0)
 lapply(2:n, function(i){vec_u[i] <<- fu(vec_u[i-1])})
 
 #create vector mean x
@@ -55,44 +68,60 @@ vec_g_noise_x <- c()
 lapply(1:n, function(i){
   vec_g_noise_x[i] <<- gaussian.noise(vec_x[i], vec_mean_x[i], vec_stddev_x[i])})
 
-#create vector xn with xn = A*x + B*u + w
 vec_xn <- c()
-lapply(1:n, function(i){vec_xn[i] <<- (A * vec_x[i] + B * vec_u[i] + vec_g_noise_x[i])})
-
-#create vector y with y = C*x + v
 vec_y <- c()
-lapply(1:n, function(i){vec_y[i] <<- (C * vec_x[i] + vec_g_noise_x[i])})
-
-#create vector pn = A * P * AT + Q
-vec_p <- c(p)
+vec_p <- c()
 vec_pn <- c()
-lapply(2:n, function(i){vec_p[i] <<- ((A * vec_p[i-1] * AT) + Q)})
-lapply(1:n, function(i){vec_pn[i] <<- ((A * vec_p[i] * AT) + Q)})
-
-#create vector K = (P * CT)/ (C * P * CT + Q)
 vec_k <- c()
-lapply(1:n, function(i){vec_k[i] <<- ((vec_p[i] * CT)/((C * vec_p[i] * CT) - Q))})
-
-#create vector yn = (y - C * x)
 vec_yn <- c()
-lapply(1:n, function(i){vec_yn[i] <<- (vec_y[i] - (C * vec_x[i]))})
-
-#create vector x_est = x + K * yn
 vec_x_est <- c()
-lapply(1:n, function(i){vec_x_est[i] <<- (vec_x[i] + (vec_k[i] * vec_yn[i]))})
-
-#create vector x_err = abs(xn.real - x_est)
-vec_x_err <- c()
-lapply(1:n, function(i){vec_x_err[i] <<- abs(vec_xn_[i] - vec_x_est[i])})
-
-#create vector p_est = (I - (K * C)) * P 
 vec_p_est <- c()
-lapply(1:n, function(i){vec_p_est[i] <<- ((I - (vec_k[i] * C)) * vec_p[i])})
+vec_x_err <- c()
+
+for (i in 1:n)
+{
+  x_prev = x_est
+  x = vec_x[i]
+  p = p_est
+  
+  #calc xn = A*x + B*u + w
+  xn = A * x + B * vec_u[i] + fw(vec_g_noise_x[i])
+  vec_xn[i] <- xn
+  
+  #calc y = H*x + v
+  y = H * x + fv(vec_g_noise_x[i])
+  vec_y[i] <- y
+  
+  #calc pn = (A * P * AT) + Q
+  pn = (A * p * AT) + Q
+  vec_p[i] <- p
+  vec_pn[i] <- pn
+  
+  #calc K = (P * HT)/ ((H * P * HT) + Q)
+  K = (p * HT) / ((H * p * HT) + Q)
+  vec_k[i] <- K
+  
+  #calc yn = (y - (H * x))
+  yn = (y - (H * x))
+  vec_yn[i] <- yn
+  
+  #x_est = x + K * yn
+  x_est <<- (x + K * yn)
+  vec_x_est[i] <- x_est
+  
+  #p_est = (I - (K * H)) * P 
+  p_est <<- ((I - (K * H)) * p)
+  vec_p_est[i] <- p_est
+  
+  #x_err = abs(xn.real - x_est)
+  x_err = abs(vec_xn_real[i] - x_est)
+  vec_x_err[i] <- x_err
+}
 
 #view data frame
 df <- data.frame(
   x = vec_x,
-  xn.real = vec_xn_,
+  xn.real = vec_xn_real,
   u = vec_u,
   x.mean = vec_mean_x,
   x.stddev = vec_stddev_x,
@@ -104,8 +133,8 @@ df <- data.frame(
   K = vec_k,
   yn = vec_yn,
   x.est = vec_x_est,
-  x.err = vec_x_err,
-  p.est = vec_p_est
+  p.est = vec_p_est,
+  x.err = vec_x_err
 )
 
 print(df)
