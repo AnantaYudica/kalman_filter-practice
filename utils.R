e278b7d9e4a2f55662be4aa995c3853ceff1ee43
Ananta Yudica <nta040689@gmail.com>
2020-03-03 15:45:06 +0700

    Fixed brackets

--- utils.R
+++ utils.R
@@ -1,6 +1,6 @@
 
 gaussian.noise <- function(v, mean_v, stddev_v)
 {
-  return(1 / ((stddev_v * sqrt(2 * pi)) *
-                exp(-((v - mean_v)^2/(2 * stddev_v ^ 2)))))
+  return((1 / ((stddev_v * sqrt(2 * pi)))) *
+                exp(-((v - mean_v)^2/(2 * (stddev_v ^ 2)))))
 }
